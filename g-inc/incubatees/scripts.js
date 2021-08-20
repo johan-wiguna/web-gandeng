4c9c9efa338e9d84bf36f23b68f35240ac36ee6a
Johan Wiguna <6181801018@student.unpar.ac.id>
2021-08-20 13:58:11 +0700

    g-inc folder

---
+++ g-inc/incubatees/scripts.js
@@ -0,0 +1,56 @@
+function initParadoxWay() {
+    "use strict";
+
+    // Cards
+    if ($(".testimonials-carousel").length > 0) {
+        var j2 = new Swiper(".testimonials-carousel .swiper-container", {
+            preloadImages: false,
+            slidesPerView: 1,
+            spaceBetween: 20,
+            loop: true,
+            grabCursor: true,
+            mousewheel: false,
+            centeredSlides: true,
+            pagination: {
+                el: '.tc-pagination',
+                clickable: true,
+                dynamicBullets: true,
+            },
+            navigation: {
+                nextEl: '.listing-carousel-button-next',
+                prevEl: '.listing-carousel-button-prev',
+            },
+            breakpoints: {
+                760: {
+                    slidesPerView: 3,
+                },
+                1024: {
+                    slidesPerView: 5,
+                },
+            }
+        });
+    }
+    
+    // Bubbles
+    // setInterval(function () {
+    //     var size = randomValue(sArray);
+    //     $('.bubbles').append('<div class="individual-bubble" style="left: ' + randomValue(bArray) + 'px; width: ' + size + 'px; height:' + size + 'px;"></div>');
+    //     $('.individual-bubble').animate({
+    //         'bottom': '100%',
+    //         'opacity': '-=0.7'
+    //     }, 4000, function () {
+    //         $(this).remove()
+    //     });
+    // }, 350);
+    
+}
+
+function zoomProfile(e) {
+    let textDiv = e.children[0].children[1]
+    console.log(textDiv)
+}
+
+//   Init All ------------------
+$(document).ready(function () {
+    initParadoxWay();
+});
