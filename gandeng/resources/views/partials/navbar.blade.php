134c085d927ad08b6bad3fe94830e7acb9a01727
Richard Lionggiwijaya <richardLionggiwijaya@hotmail.com>
2021-10-10 14:54:14 +0700

    Carousel

--- gandeng/resources/views/partials/navbar.blade.php
+++ gandeng/resources/views/partials/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-dark">
     <div class="container">
         <a class="navbar-brand" href="/">
             <img src={{ asset("img/gandeng-logotype.png") }} alt="" width="100">
@@ -8,17 +8,45 @@
         <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-            <li class="nav-item">
-            <a class=""  href="/">Test</a>
-            </li>
-            <li class="nav-item">
-            <a class="" href="/about">About</a>
-            </li>
-            <li class="nav-item">
-            <a class="" href="/blog">Blog</a>
-            
-        </ul>
+            <ul class="navbar-nav ms-auto ">
+
+                <li class="nav-item">
+                    <a class="nav-link"  href="/">Home</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="/about">About Us</a>
+                </li>
+
+                <li class="nav-item ">
+                    <a class="nav-link" href="/blog" >Departements</a>
+                </li>
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false"> Programs </a>
+                    <ul class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarScrollingDropdown">
+                        <li><a class="dropdown-item" href="#">G-Acadaemia</a></li>
+                        <li><a class="dropdown-item" href="#">G-Consulting</a></li>
+                        <li><a class="dropdown-item" href="#">G-Incubation</a></li>
+                    </ul>
+                </li>
+
+                <li class="nav-item ">
+                    <a class="nav-link" href="/blog" >Partnership</a>
+                </li>
+
+                <li class="nav-item ">
+                    <a class="nav-link" href="/blog" >Gallery</a>
+                </li>
+
+                <li class="nav-item ">
+                    <a class="nav-link" href="/blog" >Events</a>
+                </li>
+
+                <li class="nav-item ">
+                    <a class="nav-link" href="/blog" >Articles</a>
+                </li>
+            </ul>
         </div>
     </div>
-    </nav>
+</nav>
