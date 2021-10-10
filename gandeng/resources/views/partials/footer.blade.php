df59037b6811e17cd0041b03879e59e784fa7f80
Johan Wiguna <6181801018@student.unpar.ac.id>
2021-10-10 18:05:19 +0700

    Footer completed

---
+++ gandeng/resources/views/partials/footer.blade.php
@@ -0,0 +1,79 @@
+<footer class="bg-dark text-white">
+    <div class="d-flex justify-content-between">
+        <div class="footer-navigation d-flex justify-content-between w-100">
+            <div class="footer-sub-section">
+                <div class="footer-sub-title">About Us</div>
+                <ul class="footer-nav-links">
+                    <li><a href="/about/our-story">Our Story</a></li>
+                    <li><a href="/about/vision-mission">Vision & Mission</a></li>
+                    <li><a href="/about/people">People Behind Gandeng</a></li>
+                </ul>
+            </div>
+
+            <div class="footer-sub-section">
+                <div class="footer-sub-title">Programs</div>
+                <ul class="footer-nav-links">
+                    <li><a href="/about/g-academia">G-Academia</a></li>
+                    <li><a href="/about/g-consulting">G-Consulting</a></li>
+                    <li><a href="/about/g-incubation">G-Incubation</a></li>
+                </ul>
+            </div>
+
+            <div class="footer-sub-section">
+                <div class="footer-sub-title">Departments</div>
+                <ul class="footer-nav-links">
+                    <li><a href="/departments/branding">Branding</a></li>
+                    <li><a href="/departments/operations">Operations</a></li>
+                    <li><a href="/departments/strategy-growth">Strategy & Growth</a></li>
+                </ul>
+            </div>
+
+            <div class="footer-sub-section">
+                <div class="footer-sub-title">Other</div>
+                <ul class="footer-nav-links">
+                    <li><a href="/about">About Us</a></li>
+                    <li><a href="/partnership">Partnership</a></li>
+                    <li><a href="/gallery">Gallery</a></li>
+                    <li><a href="/events">Events</a></li>
+                    <li><a href="/articles">Articles</a></li>
+                </ul>
+            </div>
+
+            <div class="footer-sub-section pe-5">
+                <div class="footer-sub-title">Support</div>
+                <ul class="footer-nav-links">
+                    <li><a href="/contact">Contact Us</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="footer-newsletter ps-5">
+            <div class="footer-sub-title">
+                Subscribe to our newsletter!
+            </div>
+            <form action="" method="post">
+                <label for="newsletter-email">Email address</label>
+                <br>
+                <input class="w-100 mb-2" type="email" name="newsletter-email" id="newsletter-email" placeholder="Type your email here...">
+                
+                <button class="btn-rect bg-yellow" type="submit">Subscribe</button>
+            </form>
+        </div>
+    </div>
+
+    <div class="d-flex align-items-center justify-content-between">
+        <div id="footer-logo">
+            <img src="{{ url('img/gandeng-logotype-light.png') }}" alt="">
+        </div>
+        
+        <div id="footer-tagline" class="font-montserrat text-center"><b>#GandengSetaraTumbuhBersama</b></div>
+
+        <div id="footer-socials" class="text-end">
+            <a href="https://www.linkedin.com/company/gandengfoundation/" target="_blank"><i class="fab fa-linkedin fa-2x pe-2"></i></a>
+            <a href="https://www.instagram.com/gandengfoundation/" target="_blank"><i class="fab fa-instagram fa-2x"></i></a>
+        </div>
+    </div>
+
+    <div id="footer-copyright" class="font-montserrat text-center">
+        &copy; 2021 Gandeng Foundation
+    </div>
+</footer>
