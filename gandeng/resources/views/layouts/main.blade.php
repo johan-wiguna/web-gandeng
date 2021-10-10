61c72a2cdaf36cd5c2835f433fff4eb8732db30c
Johan Wiguna <6181801018@student.unpar.ac.id>
2021-10-10 15:17:19 +0700

    CSS link correction

--- gandeng/resources/views/layouts/main.blade.php
+++ gandeng/resources/views/layouts/main.blade.php
@@ -7,7 +7,7 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="{{ url('css/style.css') }}">
     <title>{{ $title }}</title>
   </head>
   <body>
@@ -20,5 +20,6 @@
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
 
+    @include('partials.footer')
    </body>
 </html>
