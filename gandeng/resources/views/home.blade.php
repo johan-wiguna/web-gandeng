223565afb3fa1e60d05914af1eb648df9ea764ba
Johan Wiguna <6181801018@student.unpar.ac.id>
2021-10-12 11:51:22 +0700

    Prevent carousel from pausing on hover

--- gandeng/resources/views/home.blade.php
+++ gandeng/resources/views/home.blade.php
@@ -1,13 +1,20 @@
 @extends('layouts.main')
 
 @section('content')
+<script defer>
+    $('.carousel').carousel({
+        pause: "false"
+    });
+</script>
+
 <div id="home-carousel">
-    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+    <div id="carousel-slides" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-indicators">
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            <button type="button" data-bs-target="#carousel-slides" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carousel-slides" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carousel-slides" data-bs-slide-to="2" aria-label="Slide 3"></button>
         </div>
+
         <div class="carousel-inner">
             <div class="carousel-item active">
                 <img src="../img/welcome-image1.png" class="d-block" alt="...">
@@ -19,40 +26,44 @@
                 <img src="../img/welcome-image3.png" class="d-block" alt="...">
             </div>
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+
+        <button class="carousel-control-prev" type="button" data-bs-target="#carousel-slides" data-bs-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Previous</span>
         </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+
+        <button class="carousel-control-next" type="button" data-bs-target="#carousel-slides" data-bs-slide="next">
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Next</span>
         </button>
-        </div>
+    </div>
 </div>
 
-<div id="home-gandeng-identity">
-    <div class="bg-yellow p-5">
-        <div class="container">
-            <h2 class="text-section-title text-center pb-2">Who Are We?</h2>
-            <div class="d-flex align-items-center">
-                <p class="text-justify text-content-light pe-5 pb-0">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus assumenda expedita natus officiis est hic in eos similique cum totam dolore inventore, facilis fugit saepe? Fugiat a assumenda harum vitae quibusdam minus distinctio ullam nesciunt, officia qui mollitia vel velit, ratione culpa, modi necessitatibus? Maiores earum esse quasi, nesciunt illum illo assumenda laborum quis. Cumque ab deleniti dolore accusantium nostrum itaque numquam iure vero enim, aliquam beatae alias officia blanditiis dolorem voluptate nisi tempora, culpa dolores repellat at? Rerum, placeat fugit assumenda vero omnis in minus voluptate eligendi repudiandae. Tenetur, ipsam. Quae iste aspernatur debitis fugit necessitatibus esse voluptatibus rerum?</p>
-                <img class="w-25" src="../img/welcome-image3.png" alt="">
+<div id="home-gandeng-identity" class="bg-yellow p-5">
+    <div class="container">
+        <div class="d-flex align-items-center">
+            <div class="identity-left">
+                <div class="text-section-title mb-2">Who are we?</div>
+                <p class="text-justify text-content-light pe-5 pb-0">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus assumenda expedita natus officiis est hic in eos similique cum totam dolore inventore, facilis fugit saepe? Fugiat a assumenda harum vitae quibusdam minus distinctio ullam nesciunt, officia qui mollitia vel velit, ratione culpa, modi necessitatibus? Maiores earum esse quasi, nesciunt illum illo assumenda laborum quis. Cumque ab deleniti dolore accusantium nostrum itaque numquam iure vero enim, aliquam beatae alias officia blanditiis dolorem voluptate nisi tempora, culpa dolores repellat at? Rerum, placeat fugit assumenda vero omnis in minus voluptate eligendi repudiandae. Tenetur, ipsam. Quae iste aspernatur debitis fugit necessitatibus esse voluptatibus rerum?
+                </p>
             </div>
+            
+            <img class="w-25" src="../img/welcome-image3.png" alt="">
         </div>
     </div>
 </div>
 
 <div id="home-programs">
-    <div class="container">
-        <h2 class="text-section-title text-center py-5">Our Programs</h2>
-    </div>
-    <div class="g-ac d-flex align-items-stretch">
+    <h2 class="text-section-title text-center py-5">Our Programs</h2>
+
+    <div class="program d-flex">
         <div class="col1 d-block text-center">
             <img class="w-75 align-middle" src="../img/g-academia-logotype.png" alt="">
-            <p class="text-justify text-content-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, suscipit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, ut!</p>
+            <p class="text-center text-content-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, suscipit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, ut!</p>
         </div>
         <div class="col2 bg-gac-1">
-            <h4 class="text-section-title text-center">Benefit</h4>
+            <h4 class="text-sub-section-title text-center mb-3">Benefits</h4>
             <ul class="text-content-light">
                 <li>Benefit 1 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, minima!</li>
                 <li>Benefit 1 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique, amet!</li>
@@ -60,23 +71,22 @@
             </ul>
         </div>
         <div class="col3 bg-gac-1">
-            <h4 class="text-section-title text-center">Achievements</h4>
+            <h4 class="text-sub-section-title text-center mb-3">Achievements</h4>
             <ul class="text-content-light">
                 <li>Benefit 1 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, minima!</li>
                 <li>Benefit 1 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique, amet!</li>
                 <li>Benefit 1</li>
             </ul>
         </div>
-
-            
     </div>
-    <div class="g-ac d-flex align-items-stretch">
+
+    <div class="program d-flex">
         <div class="col1 d-block text-center">
             <img class="w-75 align-middle" src="../img/g-consulting-logotype.png" alt="">
-            <p class="text-justify text-content-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, suscipit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, ut!</p>
+            <p class="text-center text-content-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, suscipit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, ut!</p>
         </div>
         <div class="col2 bg-gcon-1">
-            <h4 class="text-section-title text-center">Benefit</h4>
+            <h4 class="text-sub-section-title text-center mb-3">Benefits</h4>
             <ul class="text-content-light">
                 <li>Benefit 1 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, minima!</li>
                 <li>Benefit 1 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique, amet!</li>
@@ -84,23 +94,22 @@
             </ul>
         </div>
         <div class="col3 bg-gcon-1">
-            <h4 class="text-section-title text-center">Achievements</h4>
+            <h4 class="text-sub-section-title text-center mb-3">Achievements</h4>
             <ul class="text-content-light">
                 <li>Benefit 1 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, minima!</li>
                 <li>Benefit 1 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique, amet!</li>
                 <li>Benefit 1</li>
             </ul>
         </div>
-
-            
     </div>
-    <div class="g-ac d-flex align-items-stretch">
+
+    <div class="program d-flex">
         <div class="col1 d-block text-center">
             <img class="w-75 align-middle" src="../img/g-incubation-logotype.png" alt="">
-            <p class="text-justify text-content-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, suscipit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, ut!</p>
+            <p class="text-center text-content-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, suscipit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, ut!</p>
         </div>
         <div class="col2 bg-ginc-1">
-            <h4 class="text-section-title text-center">Benefit</h4>
+            <h4 class="text-sub-section-title text-center mb-3">Benefits</h4>
             <ul class="text-content-light">
                 <li>Benefit 1 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, minima!</li>
                 <li>Benefit 1 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique, amet!</li>
@@ -108,24 +117,14 @@
             </ul>
         </div>
         <div class="col3 bg-ginc-1">
-            <h4 class="text-section-title text-center">Achievements</h4>
+            <h4 class="text-sub-section-title text-center mb-3">Achievements</h4>
             <ul class="text-content-light">
                 <li>Benefit 1 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, minima!</li>
                 <li>Benefit 1 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique, amet!</li>
                 <li>Benefit 1</li>
             </ul>
         </div>
-
-            
     </div>
-    
-                
-        
-    </div>
-</div>
-
-<div id="home-mentor-hub">
-
 </div>
 
 <div id="home-updates">
