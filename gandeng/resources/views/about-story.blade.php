0dae11d4548ddb88abd2a5251a191e7856977ae2
Johan Wiguna <6181801018@student.unpar.ac.id>
2021-10-17 19:03:16 +0700

    about-journey section

--- gandeng/resources/views/about-story.blade.php
+++ gandeng/resources/views/about-story.blade.php
@@ -2,4 +2,23 @@
 
 @section('content')
 @include('partials.about')
+<section id="about-journey"  class="p-5">
+    <div class="container">
+        <div class="text-section-title text-center">The start of our journey</div>
+
+        <p class="text-content-regular text-center fs-5">It all started because our founders observed that there was a lack of systematic approach in soft-skill education for children in Indonesia, along with numerous social initiatives, NGO, and social enterprises who find it difficult to be sustainable, for example because of the lack of funding or human resource management strategy.</p>
+        <br>
+        <p class="text-content-regular text-center fs-5">One time, when one of our founders was mentoring a 12-year-old &#8211; trained in soft-skill and hard-skill education, a <b>simple question</b> was raised by her:</p>
+        <br>
+        <div class="text-center">
+            <q class="low-highlight font-montserrat fw-bold fs-3">Why don't you make this for other children?</q>
+        </div>
+        <br>
+        <p class="text-center fs-5">From that, our founders felt the need to step in and make an ecosystem called <q><b>(G)andeng Foundation</b></q> to help the development of students and early social enterprise/NGO.</p>
+    </div>
+</section>
+
+<section id="about-logo">
+
+</section>
 @endsection
