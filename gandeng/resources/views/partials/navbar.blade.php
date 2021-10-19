0eb9ab770cd7d3114a5ee96a57a3bb9f47b1f932
Johan Wiguna <6181801018@student.unpar.ac.id>
2021-10-19 13:03:14 +0700

    Added gandeng-logotype.svg and replace navbar logo with it

--- gandeng/resources/views/partials/navbar.blade.php
+++ gandeng/resources/views/partials/navbar.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-dark fixed-top font-montserrat px-2">
     <div class="container-fluid">
         <a class="navbar-brand" href="">
-            <img src={{ asset("img/gandeng-logos/gandeng-logotype-small.png") }} alt="Gandeng">
+            <img src={{ asset("img/gandeng-logos/gandeng-logotype.svg") }} alt="Gandeng">
         </a>
         
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
