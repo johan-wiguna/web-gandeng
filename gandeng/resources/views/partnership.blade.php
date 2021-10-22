7bcc7acaadef271c705f276d5573218edea2cd3b
Richard Lionggiwijaya <richardLionggiwijaya@hotmail.com>
2021-10-22 20:54:17 +0700

    creating form partnership registration (unfinished)

--- gandeng/resources/views/partnership.blade.php
+++ gandeng/resources/views/partnership.blade.php
@@ -9,7 +9,7 @@
     
 </div>
 <section id="partners">
-    <div class="container text-center p-5">
+    <div class="text-center p-5">
         <div class="text-section-title">Program partnership</div>
         <p class="text-content-regular">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus exercitationem error vitae? Numquam ipsam quam ullam excepturi nihil temporibus sed facilis voluptate odit. Debitis, et possimus nesciunt facere eveniet vel quidem id amet veritatis, adipisci, nisi nemo neque sapiente sequi!</p>
     
@@ -23,6 +23,12 @@
             <div class="logo">
                 <img src="{{ asset('../img/sample-image-4.jpg') }}"  alt="">
             </div>
+            <div class="logo">
+                <img src="{{ asset('../img/sample-image-4.jpg') }}"  alt="">
+            </div>
+            <div class="logo">
+                <img src="{{ asset('../img/sample-image-4.jpg') }}"  alt="">
+            </div>
         </div>
         <div class="d-flex justify-content-center">
             <div class="logo">
@@ -34,7 +40,28 @@
             <div class="logo">
                 <img src="{{ asset('../img/sample-image-4.jpg') }}"  alt="">
             </div>
+            <div class="logo">
+                <img src="{{ asset('../img/sample-image-4.jpg') }}"  alt="">
+            </div>
+            <div class="logo">
+                <img src="{{ asset('../img/sample-image-4.jpg') }}"  alt="">
+            </div>
         </div>
+    </div>
+</section>
+<section id="partnership-registration">
+    <div class="p-5">
+        <div class="text-center">
+            <div class="text-section-title">Register NOW!</div>
+        <p class="text-content-regular">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus exercitationem error vitae? Numquam ipsam quam ullam excepturi nihil temporibus sed facilis voluptate odit. Debitis, et possimus nesciunt facere eveniet vel quidem id amet veritatis, adipisci, nisi nemo neque sapiente sequi!</p>
+        </div>
+        <div class="box bg-yellow p-5">
+            <form action="" class="bg-yellow p-5">
+            <div class="text-section-title">Register NOW!</div>
+            <label for="">Full Name</label>
+        </form>
+        </div>
+        
     </div>
 </section>
 
