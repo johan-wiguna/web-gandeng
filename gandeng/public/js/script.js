87f2a396c5caef09cd2715a03ae6f6ff6cb1932a
Johan Wiguna <6181801018@student.unpar.ac.id>
2021-10-31 21:57:22 +0700

    Departments member template

--- gandeng/public/js/script.js
+++ gandeng/public/js/script.js
@@ -1,7 +1,35 @@
 /* HOME PAGE */
 /* carousel items*/
-$('.carousel').carousel({
-        pause: "false"
-});
+// $('.carousel').carousel({
+//         pause: "false"
+// });
 
+function changeDepartment(id) {
+    var selected = document.getElementById(id)
+    var buttonParent = selected.parentElement
+    var buttons = buttonParent.children
 
+    var memberParent = document.getElementById('department-container')
+    var members = memberParent.children
+    var selectedIdx
+
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].classList.remove('nav-active')
+        
+        if (buttons[i].id === id) {
+            selectedIdx = i
+        }
+    }
+
+    console.log(members)
+
+    for (var i = 0; i < members.length; i++) {
+        members[i].classList.add('d-none')
+
+        if (selectedIdx === i) {
+            members[i].classList.remove('d-none')
+        }
+    }
+
+    selected.classList.add('nav-active')
+}
--- gandeng/resources/views/departments.blade.php
+++ gandeng/resources/views/departments.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.main')
 
 @section('content')
+
 <div class="bg-dark">
     <div class="container p-5 text-light text-center">
     
@@ -15,9 +16,28 @@
 </div>
 <div class="bg-light">
     <div class="container p-5">
-        <p class=" text-content-regular-l pb-0">Purus faucibus lobortis ac dolor eget orci, lectus platea. Integer in feugiat viverra enim. Magna lectus ut libero turpis elit, tempus lectus sem. Ut sem consequat porttitor mauris arcu, mattis suspendisse egestas. Dignissim at nibh blandit mollis tellus aliquam volutpat, vestibulum.</p>
+        <p class=" text-content-regular-l text-center pb-0">Purus faucibus lobortis ac dolor eget orci, lectus platea. Integer in feugiat viverra enim. Magna lectus ut libero turpis elit, tempus lectus sem. Ut sem consequat porttitor mauris arcu, mattis suspendisse egestas. Dignissim at nibh blandit mollis tellus aliquam volutpat, vestibulum.</p>
+
+        <div id="departments-navigation">
+            <button id="nav-branding" class="nav-active" onclick="changeDepartment(this.id)">Branding</button>
+            <button id="nav-operations" onclick="changeDepartment(this.id)">Operations</button>
+            <button id="nav-sng" onclick="changeDepartment(this.id)">Strategy & Growth</button>
+        </div>
+        
+        <div id="department-container">
+            <div id="branding-members">
+                <p>1 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit ratione, eveniet quo et iste ipsam odio harum temporibus similique adipisci assumenda non voluptates? Labore, mollitia quam! Fugiat quis nulla accusamus.</p>
+            </div>
+            
+            <div id="operations-members" class="d-none">
+                <p>2 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit ratione, eveniet quo et iste ipsam odio harum temporibus similique adipisci assumenda non voluptates? Labore, mollitia quam! Fugiat quis nulla accusamus.</p>
+            </div>
+
+            <div id="sng-members" class="d-none">
+                <p>3 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit ratione, eveniet quo et iste ipsam odio harum temporibus similique adipisci assumenda non voluptates? Labore, mollitia quam! Fugiat quis nulla accusamus.</p>
+            </div>
+        </div>
     </div>
-    
 </div>
 
 @endsection
