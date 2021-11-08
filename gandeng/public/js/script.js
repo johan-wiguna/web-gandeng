aaf1be79f0ea4014be7730fc45b818cc129a4cae
Johan Wiguna <6181801018@student.unpar.ac.id>
2021-11-08 10:24:38 +0700

    Fixed script indent

--- gandeng/public/js/script.js
+++ gandeng/public/js/script.js
@@ -2,31 +2,29 @@
 /* carousel items*/
 
 function switchAbout(id){
-        var selected = document.getElementById(id)
-        var navParent = selected.parentElement //ul
-        var navs = navParent.children //li
+    var selected = document.getElementById(id)
+    var navParent = selected.parentElement
+    var navs = navParent.children
 
-        var contentParent = document.getElementById('content-parent') //content-parent
-        var contents = contentParent.children //section founder-vm-story
-        var selectedIdx
+    var contentParent = document.getElementById('content-parent')
+    var contents = contentParent.children
+    var selectedIdx
 
-        for (var i = 0; i<navs.length; i++){
-                navs[i].classList.remove('nav-active')
+    for (var i = 0; i < navs.length; i++){
+        navs[i].classList.remove('nav-active')
 
-                if(navs[i].id === id){
-                        selectedIdx = i
-                }
+        if(navs[i].id === id){
+            selectedIdx = i
         }
+    }
 
-        selected.classList.add('nav-active')
+    selected.classList.add('nav-active')
 
-        for (var i = 0; i<contents.length; i++){
-                contents[i].classList.add('d-none')
+    for (var i = 0; i < contents.length; i++){
+        contents[i].classList.add('d-none')
 
-                if(selectedIdx === i){
-                        contents[i].classList.remove('d-none')        
-                }
+        if(selectedIdx === i){
+            contents[i].classList.remove('d-none')        
         }
-        
-
+    }
 }
